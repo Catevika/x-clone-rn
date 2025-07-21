e3a7e04868a2369f5eb236cf9db125378db4a981
Catevika <dominique.bello@outlook.fr>
2025-07-21 10:01:11 +0200

    Add DB models

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import {connectDB} from './config/db.js';
+import {ENV} from './config/env.js';
 
 const app = express();
 
-app.listen(5001, () => console.log('Server is up and running on port 5001'));
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello from Server'));
+
+app.listen(ENV.PORT, () =>
+	console.log('⭐ Server is up and running on port: ', ENV.PORT),
+);

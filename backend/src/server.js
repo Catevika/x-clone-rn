a58a481c1999bcb5d164d2a9472926605c9b4ac8
Catevika <dominique.bello@outlook.fr>
2025-07-21 12:30:03 +0200

    Add user routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
+import {clerkMiddleware} from '@clerk/express';
+import cors from 'cors';
 import express from 'express';
 import {connectDB} from './config/db.js';
 import {ENV} from './config/env.js';
+import userRoutes from './routes/user.route.js';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from Server'));
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.get('/', (res) => res.send('Hello from Server'));
+
+app.use('/api/users', userRoutes);
 
 const startServer = async () => {
 	try {
@@ -13,7 +22,7 @@ const startServer = async () => {
 			console.log('⭐ Server is up and running on port: ', ENV.PORT),
 		);
 	} catch (error) {
-		console.error('❌ Failed to start the server:', error);
+		console.error('❌ Failed to start the server: ', error);
 		process.exit(1);
 	}
 };

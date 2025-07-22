27a7ef9509d4a485825bc1f044285798cf158bd0
Catevika <dominique.bello@outlook.fr>
2025-07-22 13:50:50 +0200

    Fix arcjet import in server file

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ import userRoutes from './routes/user.route.js';
 
 import {connectDB} from './config/db.js';
 import {ENV} from './config/env.js';
-import {arcjetMiddleware} from './middleware/arcjet.middleware.js';
+import {arcjet} from './middleware/arcjet.middleware.js';
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
-app.use(arcjetMiddleware);
+app.use(arcjet);
 
 app.get('/', (req, res) => res.send('Hello from server'));
 

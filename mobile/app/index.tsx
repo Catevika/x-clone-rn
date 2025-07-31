db48252978dc93b29842d349f009bf5600d0d711
Catevika <dominique.bello@outlook.fr>
2025-07-31 11:12:47 +0200

    Add Clerk auth

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,16 @@
-import {Text, View} from 'react-native';
+import {useClerk} from '@clerk/clerk-expo';
+import {Button, Text, View} from 'react-native';
 
-export default function Index() {
+const HomeScreen = () => {
+	const {signOut} = useClerk();
 	return (
-		<View className='flex-1 items-center justify-center bg-white'>
-			<Text className='text-xl font-bold text-blue-500'>Hello World</Text>
+		<View>
+			<Text>Home screen</Text>
+			<Button
+				onPress={() => signOut()}
+				title='Sign Out'
+			/>
 		</View>
 	);
-}
+};
+export default HomeScreen;
